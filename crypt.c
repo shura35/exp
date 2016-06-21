2c5ca9260ec9fcedaec99f0ca903036936c7ba7b
shura35 <guillaume.laumaillet@orange.fr>
2016-06-21 15:35:50 +0200

    rand

--- crypt.c
+++ crypt.c
@@ -4,7 +4,7 @@
 #include <time.h>
 
 #define KEY_SIZE 32
-#define BUFF_SIZE 8192
+#define BUFF_SIZE 1024
 
 unsigned int holdrand = 0;
 
@@ -32,7 +32,7 @@ char* genere_key(void) {
   return key;
 }
 
-void crypt_buffer(unsigned char *bufferin, unsigned char *bufferout,  size_t size, char *key) {
+void crypt_buffer(unsigned char *buffer, size_t size, char *key) {
   size_t i;
   int j;
 
@@ -40,75 +40,61 @@ void crypt_buffer(unsigned char *bufferin, unsigned char *bufferout,  size_t siz
   for(i = 0; i < size; i++) {
     if(j >= KEY_SIZE)
       j = 0;
-    bufferout[i] = bufferin[i] ^ key[j];
+    buffer[i] ^= key[j];
     j++;
   }
 }
 
-void crypt_file(unsigned char *filename, unsigned char *bufferin, size_t size) {
+void crypt_file(FILE *in, FILE *out) {
+  unsigned char buffer[BUFF_SIZE];
   char *key;
-  unsigned char bufferout[BUFF_SIZE];
+  size_t size;
 
-  unsigned int begin = 1354320000; // 01/12/2012
-  unsigned int end   = 1356998400; // 01/01/2013
-  unsigned int i = begin;
+  key = genere_key();
 
-  for(; i < end; i++) {
+  printf("[+] Using key : %s\n", key);
 
-      Srand(i);
+  do {
+    size = fread(buffer, 1, BUFF_SIZE, in);
+    crypt_buffer(buffer, size, key);
+    fwrite(buffer, 1, size, out);
 
-      key = genere_key();
-
-      crypt_buffer(bufferin, bufferout, size, key);
-
-        if (i % 100000 == 0) {
-            printf("[+] trying key : %s\n", key);
-        }
-
-      if (bufferout[0] == 'B' && bufferout[1] == 'Z' && bufferout[2] == 'h') {
-          char path[128];
-          FILE *out;
-          printf("[+] File header may be bzip2ped... Used key : %s\n", key);
-
-          snprintf(path, sizeof(path)-1, "%s.%i", filename, i);
-          if((out = fopen(path, "w")) == NULL) {
-              perror("[-] fopen (out) ");
-              exit(EXIT_FAILURE);
-          }
-          fwrite(bufferout, 1, size, out);
-          fclose(out);
-      }
-
-  }
-
+  }while(size == BUFF_SIZE);  
 }
 
 int main(int argc, char **argv) {
-  FILE *in;
+  char path[128];
+  FILE *in, *out;
+  char *key;
+	
+  key = genere_key();
+  printf("!!%s\n", key);
+  
+  Srand(time(NULL));
 
-
+  key = genere_key();
+  printf("!!%s\n\n", key);
+  
   if(argc != 2) {
     printf("[-] Usage : %s <file>\n", argv[0]);
     return EXIT_FAILURE;
   }
 
+  snprintf(path, sizeof(path)-1, "%s.crypt", argv[1]);
 
   if((in = fopen(argv[1], "r")) == NULL) {
     perror("[-] fopen (in) ");
     return EXIT_FAILURE;
   }
 
-  unsigned char buffer[BUFF_SIZE];
-  size_t size;
-  size = fread(buffer, 1, BUFF_SIZE, in);
-  fclose(in);
-
-  if (size == BUFF_SIZE) {
-      exit(5); // BUFFER TOO SHORT !
+  if((out = fopen(path, "w")) == NULL) {
+    perror("[-] fopen (out) ");
+    return EXIT_FAILURE;
   }
 
-  crypt_file(argv[1], buffer, size);
+  crypt_file(in, out);
 
+  printf("[+] File %s crypted !\n", path);
   printf("[+] DONE.\n");
   return EXIT_SUCCESS;
 }
